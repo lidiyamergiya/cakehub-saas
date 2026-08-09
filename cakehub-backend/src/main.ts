a604d431cabdd75883d0a226a0595823686d6055
Lidiya Mergiya <mergiyalidiya@gmail.com>
2026-08-09 23:05:47 +0300

    chore: update NestJS bootstrap configuration

--- cakehub-backend/src/main.ts
+++ cakehub-backend/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,4 +21,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
